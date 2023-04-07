0a8f6d1975b3221818e5cd07bd08aec05885a254
Documenter.jl <documenter@juliadocs.github.io>
2023-04-07 17:22:17 +0000

    build based on bd32b56

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#ReadForge.jl","page":"Home","title":"ReadForge.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Welcome to the ReadForge.jl documentation!","category":"page"},{"location":"","page":"Home","title":"Home","text":"testing","category":"page"}]
+[{"location":"#ReadForge.jl","page":"Home","title":"ReadForge.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Greetings, and welcome to the ReadForge.jl documentation!","category":"page"}]
 }
