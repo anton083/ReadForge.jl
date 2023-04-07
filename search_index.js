e0e54170a1ce8050797559d089b7eb4747d40935
Periareion <periareion05@gmail.com>
2023-04-07 19:40:55 +0200

    Removed unused files and directories

--- search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"#ReadForge.jl","page":"Home","title":"ReadForge.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Welcome to the ReadForge.jl documentation!","category":"page"}]
-}
