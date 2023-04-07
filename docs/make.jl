2f671b118cc6f97791ce33377e22965af8344d6a
Periareion <periareion05@gmail.com>
2023-04-07 18:49:52 +0200

    fix

--- docs/make.jl
+++ docs/make.jl
@@ -1,20 +1,8 @@
 using Documenter
 using ReadForge
 
-makedocs(
-    sitename = "ReadForge.jl",
-    modules = [ReadForge],
-    format = Documenter.HTML(),
-    pages = [
-        "Home" => "index.md"
-    ],
-    repo="https://github.com/Periareion/ReadForge.jl.git"
-)
+makedocs()
 
 deploydocs(
-    repo = "github.com/Periareion/ReadForge.jl.git", # Replace 'username' with your GitHub username
-    target = "build",
-    branch = "gh-pages",
-    devbranch = "dev", # The branch that you want to build the documentation from
-    push_preview = true,
+    repo = "github.com/Periareion/ReadForge.jl.git",
 )
