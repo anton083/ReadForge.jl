a9613b262d939e567118aa3774313cde9899553c
Periareion <periareion05@gmail.com>
2023-04-07 18:23:00 +0200

    fix

--- docs/make.jl
+++ docs/make.jl
@@ -16,8 +16,6 @@ deploydocs(
     target = "build",
     branch = "gh-pages",
     devbranch = "dev", # The branch that the documentation is built from
-    julia = "1.8",
-    ssh = raw"GITHUB_TOKEN", # Use "DOCUMENTER_KEY" if you have set up an SSH deploy key
     push_preview = true,
     make = x->nothing,
 )
