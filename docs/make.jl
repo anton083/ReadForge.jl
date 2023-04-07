0742641f6eeace868fb150b88f7d069b93170ef0
Periareion <periareion05@gmail.com>
2023-04-07 19:02:26 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,23 @@
 using Documenter
 using ReadForge
 
-makedocs()
+makedocs(
+    sitename="ReadForge.jl Documentation",
+    modules=[ReadForge],
+    format=Documenter.HTML(),
+    pages=[
+        "Home" => "index.md",
+        "API" => "api.md",
+    ],
+    authors="Your Name",
+    assets=["assets"],
+)
 
 deploydocs(
-    repo = "github.com/Periareion/ReadForge.jl.git",
+    repo="github.com/Periareion/ReadForge.jl.git",
+    push_preview=true,
+    devbranch="dev",
+    devurl="dev/",
+    target="build",
+    branch="gh-pages",
 )
