8836006c6a329e7e71c5caf6c81a3c9f6f8ae5e7
Periareion <periareion05@gmail.com>
2023-04-08 14:24:50 +0200

    Added Genome docs

--- docs/src/index.md
+++ docs/src/index.md
@@ -1,3 +1,7 @@
 # ReadForge.jl
 
 Greetings, and welcome to the ReadForge.jl documentation!
+
+```@docs
+ReadForge.Genome
+```
--- src/Sequencing/genome.jl
+++ src/Sequencing/genome.jl
@@ -1,7 +1,8 @@
 
-using BioSequences
+export
+    Genome
 
-export Genome
+using BioSequences
 
 struct Genome
     sequence::LongDNA{4}
@@ -10,6 +11,11 @@ struct Genome
     GC_content::Float64
 end
 
+"""
+    Genome(sequence::LongDNA{4})
+
+Create a genome from a sequence. The genome struct stores the reverse complement sequence, length, and GC-content. 
+"""
 function Genome(sequence::LongDNA{4})
     Genome(sequence, reverse_complement(sequence), length(sequence), BioSequences.gc_content(sequence))
 end
